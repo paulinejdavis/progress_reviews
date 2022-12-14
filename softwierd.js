4d8e1df53aa26dc8285c6441b1a41bd39e0a66eb
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-14 10:59:45 +0000

    passing test on single a

---
+++ softwierd.js
@@ -0,0 +1,8 @@
+// const dict = ['The', 'fox', 'jumped', 'a']
+
+const softwierd = (word) => {
+    return "a"
+
+}
+
+module.exports = softwierd;
--- softwierd.test.js
+++ softwierd.test.js
@@ -2,7 +2,7 @@ const softwierd = require('./softwierd.js');
 
 describe('softwierd method', () => {
     it ('return "a" when given "a"', () => {
-        expect(softwierd("a")).toEqual('a');
+        expect(softwierd("a")).toEqual("a");
 
     }); 
 });
